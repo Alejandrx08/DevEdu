ec2c1c9407be93e7b1e083279e54817cad3cc49b
Alejandrobethancourth <harryibarra43@gmail.com>
2025-11-16 22:09:32 -0600

    16/11, DevVis Ex2 End

--- DevEdu/Login V1/DevVis_ESTDTS_ej2.Designer.cs
+++ DevEdu/Login V1/DevVis_ESTDTS_ej2.Designer.cs
@@ -34,7 +34,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtbx_slctnodo = new System.Windows.Forms.TextBox();
             this.txtbx_valornodo = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -42,13 +41,30 @@
             this.btn_restart = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.flecha1 = new System.Windows.Forms.Label();
+            this.flecha3 = new System.Windows.Forms.Label();
+            this.flecha2 = new System.Windows.Forms.Label();
+            this.flecha4 = new System.Windows.Forms.Label();
+            this.flecha6 = new System.Windows.Forms.Label();
+            this.flecha5 = new System.Windows.Forms.Label();
+            this.bx1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bx2 = new System.Windows.Forms.Button();
+            this.bx4 = new System.Windows.Forms.Button();
+            this.bx6 = new System.Windows.Forms.Button();
+            this.bx3 = new System.Windows.Forms.Button();
+            this.bx5 = new System.Windows.Forms.Button();
+            this.bx7 = new System.Windows.Forms.Button();
+            this.btn_return = new System.Windows.Forms.Button();
+            this.txtbx_slctnodo = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Navy;
+            this.pictureBox1.BackColor = System.Drawing.Color.MidnightBlue;
             this.pictureBox1.Location = new System.Drawing.Point(-6, 1);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(246, 450);
@@ -58,7 +74,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.BackColor = System.Drawing.Color.MidnightBlue;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(49, 7);
@@ -100,13 +116,6 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "hasta ese nodo.\r\n";
             // 
-            // txtbx_slctnodo
-            // 
-            this.txtbx_slctnodo.Location = new System.Drawing.Point(40, 142);
-            this.txtbx_slctnodo.Name = "txtbx_slctnodo";
-            this.txtbx_slctnodo.Size = new System.Drawing.Size(162, 22);
-            this.txtbx_slctnodo.TabIndex = 7;
-            // 
             // txtbx_valornodo
             // 
             this.txtbx_valornodo.Location = new System.Drawing.Point(40, 201);
@@ -117,7 +126,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Navy;
+            this.label5.BackColor = System.Drawing.Color.MidnightBlue;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(57, 119);
@@ -129,7 +138,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Navy;
+            this.label6.BackColor = System.Drawing.Color.MidnightBlue;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(68, 177);
@@ -162,18 +171,19 @@
             this.btn_restart.TabIndex = 15;
             this.btn_restart.Text = "restablecer ejemplo";
             this.btn_restart.UseVisualStyleBackColor = false;
+            this.btn_restart.Click += new System.EventHandler(this.btn_restart_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Navy;
+            this.label7.BackColor = System.Drawing.Color.MidnightBlue;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
             this.label7.Location = new System.Drawing.Point(12, 401);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(223, 40);
+            this.label7.Size = new System.Drawing.Size(226, 40);
             this.label7.TabIndex = 16;
-            this.label7.Text = "El arbol es generico el layout se \r\n     genera automaticamente";
+            this.label7.Text = "El arbol busca la ruta mas rapida\r\n               hacia la raiz";
             // 
             // groupBox1
             // 
@@ -186,12 +196,213 @@
             this.groupBox1.TabIndex = 17;
             this.groupBox1.TabStop = false;
             // 
+            // flecha1
+            // 
+            this.flecha1.AutoSize = true;
+            this.flecha1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.flecha1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flecha1.ForeColor = System.Drawing.Color.White;
+            this.flecha1.Location = new System.Drawing.Point(419, 85);
+            this.flecha1.Name = "flecha1";
+            this.flecha1.Size = new System.Drawing.Size(31, 50);
+            this.flecha1.TabIndex = 25;
+            this.flecha1.Text = "|";
+            // 
+            // flecha3
+            // 
+            this.flecha3.AutoSize = true;
+            this.flecha3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.flecha3.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flecha3.ForeColor = System.Drawing.Color.White;
+            this.flecha3.Location = new System.Drawing.Point(419, 211);
+            this.flecha3.Name = "flecha3";
+            this.flecha3.Size = new System.Drawing.Size(31, 50);
+            this.flecha3.TabIndex = 26;
+            this.flecha3.Text = "|";
+            // 
+            // flecha2
+            // 
+            this.flecha2.AutoSize = true;
+            this.flecha2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.flecha2.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flecha2.ForeColor = System.Drawing.Color.White;
+            this.flecha2.Location = new System.Drawing.Point(430, 72);
+            this.flecha2.Name = "flecha2";
+            this.flecha2.Size = new System.Drawing.Size(172, 50);
+            this.flecha2.TabIndex = 27;
+            this.flecha2.Text = "__________";
+            // 
+            // flecha4
+            // 
+            this.flecha4.AutoSize = true;
+            this.flecha4.BackColor = System.Drawing.Color.MidnightBlue;
+            this.flecha4.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flecha4.ForeColor = System.Drawing.Color.White;
+            this.flecha4.Location = new System.Drawing.Point(430, 211);
+            this.flecha4.Name = "flecha4";
+            this.flecha4.Size = new System.Drawing.Size(172, 50);
+            this.flecha4.TabIndex = 28;
+            this.flecha4.Text = "__________";
+            // 
+            // flecha6
+            // 
+            this.flecha6.AutoSize = true;
+            this.flecha6.BackColor = System.Drawing.Color.MidnightBlue;
+            this.flecha6.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flecha6.ForeColor = System.Drawing.Color.White;
+            this.flecha6.Location = new System.Drawing.Point(623, 296);
+            this.flecha6.Name = "flecha6";
+            this.flecha6.Size = new System.Drawing.Size(56, 100);
+            this.flecha6.TabIndex = 29;
+            this.flecha6.Text = "\\\r\n  \\";
+            // 
+            // flecha5
+            // 
+            this.flecha5.AutoSize = true;
+            this.flecha5.BackColor = System.Drawing.Color.MidnightBlue;
+            this.flecha5.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flecha5.ForeColor = System.Drawing.Color.White;
+            this.flecha5.Location = new System.Drawing.Point(626, 86);
+            this.flecha5.Name = "flecha5";
+            this.flecha5.Size = new System.Drawing.Size(76, 150);
+            this.flecha5.TabIndex = 30;
+            this.flecha5.Text = "\\\r\n  \\\r\n    \\";
+            // 
+            // bx1
+            // 
+            this.bx1.BackColor = System.Drawing.Color.White;
+            this.bx1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.bx1.ForeColor = System.Drawing.Color.Black;
+            this.bx1.Location = new System.Drawing.Point(399, 19);
+            this.bx1.Name = "bx1";
+            this.bx1.Size = new System.Drawing.Size(71, 71);
+            this.bx1.TabIndex = 33;
+            this.bx1.Text = "A";
+            this.bx1.UseVisualStyleBackColor = false;
+            this.bx1.Click += new System.EventHandler(this.bx1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(246, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(542, 426);
+            this.pictureBox2.TabIndex = 31;
+            this.pictureBox2.TabStop = false;
+            // 
+            // bx2
+            // 
+            this.bx2.BackColor = System.Drawing.Color.White;
+            this.bx2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.bx2.ForeColor = System.Drawing.Color.Black;
+            this.bx2.Location = new System.Drawing.Point(399, 138);
+            this.bx2.Name = "bx2";
+            this.bx2.Size = new System.Drawing.Size(71, 71);
+            this.bx2.TabIndex = 34;
+            this.bx2.Text = "B";
+            this.bx2.UseVisualStyleBackColor = false;
+            this.bx2.Click += new System.EventHandler(this.bx2_Click);
+            // 
+            // bx4
+            // 
+            this.bx4.BackColor = System.Drawing.Color.White;
+            this.bx4.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.bx4.ForeColor = System.Drawing.Color.Black;
+            this.bx4.Location = new System.Drawing.Point(399, 264);
+            this.bx4.Name = "bx4";
+            this.bx4.Size = new System.Drawing.Size(71, 71);
+            this.bx4.TabIndex = 35;
+            this.bx4.Text = "D";
+            this.bx4.UseVisualStyleBackColor = false;
+            this.bx4.Click += new System.EventHandler(this.bx4_Click);
+            // 
+            // bx6
+            // 
+            this.bx6.BackColor = System.Drawing.Color.White;
+            this.bx6.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.bx6.ForeColor = System.Drawing.Color.Black;
+            this.bx6.Location = new System.Drawing.Point(672, 192);
+            this.bx6.Name = "bx6";
+            this.bx6.Size = new System.Drawing.Size(71, 71);
+            this.bx6.TabIndex = 36;
+            this.bx6.Text = "F";
+            this.bx6.UseVisualStyleBackColor = false;
+            this.bx6.Click += new System.EventHandler(this.bx6_Click);
+            // 
+            // bx3
+            // 
+            this.bx3.BackColor = System.Drawing.Color.White;
+            this.bx3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.bx3.ForeColor = System.Drawing.Color.Black;
+            this.bx3.Location = new System.Drawing.Point(585, 71);
+            this.bx3.Name = "bx3";
+            this.bx3.Size = new System.Drawing.Size(71, 71);
+            this.bx3.TabIndex = 37;
+            this.bx3.Text = "C";
+            this.bx3.UseVisualStyleBackColor = false;
+            this.bx3.Click += new System.EventHandler(this.bx3_Click);
+            // 
+            // bx5
+            // 
+            this.bx5.BackColor = System.Drawing.Color.White;
+            this.bx5.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.bx5.ForeColor = System.Drawing.Color.Black;
+            this.bx5.Location = new System.Drawing.Point(561, 232);
+            this.bx5.Name = "bx5";
+            this.bx5.Size = new System.Drawing.Size(71, 71);
+            this.bx5.TabIndex = 38;
+            this.bx5.Text = "E";
+            this.bx5.UseVisualStyleBackColor = false;
+            this.bx5.Click += new System.EventHandler(this.bx5_Click);
+            // 
+            // bx7
+            // 
+            this.bx7.BackColor = System.Drawing.Color.White;
+            this.bx7.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.bx7.ForeColor = System.Drawing.Color.Black;
+            this.bx7.Location = new System.Drawing.Point(645, 345);
+            this.bx7.Name = "bx7";
+            this.bx7.Size = new System.Drawing.Size(71, 71);
+            this.bx7.TabIndex = 39;
+            this.bx7.Text = "G";
+            this.bx7.UseVisualStyleBackColor = false;
+            this.bx7.Click += new System.EventHandler(this.bx7_Click);
+            // 
+            // btn_return
+            // 
+            this.btn_return.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(31)))), ((int)(((byte)(84)))));
+            this.btn_return.ForeColor = System.Drawing.Color.White;
+            this.btn_return.Location = new System.Drawing.Point(713, 19);
+            this.btn_return.Name = "btn_return";
+            this.btn_return.Size = new System.Drawing.Size(75, 23);
+            this.btn_return.TabIndex = 54;
+            this.btn_return.Text = "return";
+            this.btn_return.UseVisualStyleBackColor = false;
+            this.btn_return.Click += new System.EventHandler(this.btn_return_Click);
+            // 
+            // txtbx_slctnodo
+            // 
+            this.txtbx_slctnodo.Location = new System.Drawing.Point(40, 142);
+            this.txtbx_slctnodo.Name = "txtbx_slctnodo";
+            this.txtbx_slctnodo.Size = new System.Drawing.Size(162, 22);
+            this.txtbx_slctnodo.TabIndex = 55;
+            // 
             // DevVis_ESTDTS_ej2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(31)))), ((int)(((byte)(84)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtbx_slctnodo);
+            this.Controls.Add(this.btn_return);
+            this.Controls.Add(this.bx7);
+            this.Controls.Add(this.bx5);
+            this.Controls.Add(this.bx3);
+            this.Controls.Add(this.bx6);
+            this.Controls.Add(this.bx4);
+            this.Controls.Add(this.bx2);
+            this.Controls.Add(this.bx1);
+            this.Controls.Add(this.flecha3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label7);
@@ -200,8 +411,13 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtbx_valornodo);
-            this.Controls.Add(this.txtbx_slctnodo);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flecha1);
+            this.Controls.Add(this.flecha2);
+            this.Controls.Add(this.flecha4);
+            this.Controls.Add(this.flecha6);
+            this.Controls.Add(this.flecha5);
+            this.Controls.Add(this.pictureBox2);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DevVis_ESTDTS_ej2";
@@ -210,6 +426,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -222,7 +439,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtbx_slctnodo;
         private System.Windows.Forms.TextBox txtbx_valornodo;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
@@ -230,5 +446,21 @@
         private System.Windows.Forms.Button btn_restart;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label flecha1;
+        private System.Windows.Forms.Label flecha3;
+        private System.Windows.Forms.Label flecha2;
+        private System.Windows.Forms.Label flecha4;
+        private System.Windows.Forms.Label flecha6;
+        private System.Windows.Forms.Label flecha5;
+        private System.Windows.Forms.Button bx1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button bx2;
+        private System.Windows.Forms.Button bx4;
+        private System.Windows.Forms.Button bx6;
+        private System.Windows.Forms.Button bx3;
+        private System.Windows.Forms.Button bx5;
+        private System.Windows.Forms.Button bx7;
+        private System.Windows.Forms.Button btn_return;
+        private System.Windows.Forms.TextBox txtbx_slctnodo;
     }
 }
